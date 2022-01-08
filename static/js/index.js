ed7dafbe7dc1b860eea5d4c025e86a12994e7bf2
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-08 15:57:21 +0530

    Changes

--- static/js/index.js
+++ static/js/index.js
@@ -24,9 +24,33 @@ function toggleMenubar(e) {
 	}
 }
 
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 14, 2022 18:55:25").getTime();
 
-$("#countdown").countdown("2021/12/29 14:30:00").on('update.countdown', function(event) {
-		var $this = $(this).html(
-			event.strftime('<div>%D <span>Days</span></div>  <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>')
-		);
-	});
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
