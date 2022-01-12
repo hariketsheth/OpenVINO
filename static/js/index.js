23cffef66351a8c7ad5b340bb5c03d9e03eca655
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2022-01-12 12:48:40 +0530

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -25,7 +25,7 @@ function toggleMenubar(e) {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 12, 2022 12:48:10").getTime();
+var countDownDate = new Date("Jan 14, 2022 18:56:10").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
